27ba5c22563788545e9baac834da2ad6f45452c9
sophialadwiniec <slladwiniec@dons.usfca.edu>
2021-01-27 20:49:15 -0800

    added Hello World files

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  textSize(40); 
+  textAlign(CENTER); 
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+  fill(64, 224, 208); 
+  // width and height for numbers 
+  text("Hello, World", width/2, height/2); 
+}
